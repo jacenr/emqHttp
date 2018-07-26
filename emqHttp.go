f810ff158372712dbc4580c0ce4b240af962ac17
zhicun_ren <jacenr@sc24358.xd-ad.com.cn>
2018-07-26 16:02:47 +0800

    feat: http

---
+++ emqHttp.go
@@ -0,0 +1,37 @@
+package emqhttp
+
+import (
+	"log"
+	"net/http"
+)
+
+type account map[string]string
+
+func (ac *account) auth(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal("Can't parse post data.")
+	}
+	username := req.Form.Get("u")
+	passwd := req.Form.Get("p")
+	if username == ""|passwd == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	pwd, ok := ac["username"]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if passwd != pwd {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	ac := account{"tom": "tompasswd"}
+	http.HandleFunc("/auth", ac.auth)
+	log.Fatal(http.ListenAndServe(":8082", nil))
+}
